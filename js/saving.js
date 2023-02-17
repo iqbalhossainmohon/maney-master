e70e20830e75b72bd17da749fdbd5c3fa1944f8c
iqbalhossainmohon <iqbalhossainmohon3057@gmail.com>
2023-02-17 11:18:32 +0600

    validation

--- js/saving.js
+++ js/saving.js
@@ -5,21 +5,41 @@ document.getElementById('btn-calculate').addEventListener ('click',function(){
     const incomeField = document.getElementById('income-field');
     const incomeFieldString = incomeField.value;
     const incomeFieldNumber = parseInt(incomeFieldString);
+
+    if(isNaN(incomeFieldNumber)){
+        alert('Please provide a valid number');
+        return;
+    }
     
     // food Field
     const foodField = document.getElementById('food-field');
     const foodFieldString = foodField.value;
     const foodFieldNumber = parseInt(foodFieldString);
 
+    if(isNaN(foodFieldNumber)){
+        alert('Please provide a valid number');
+        return;
+    }
+
     // rent Field
     const rentField = document.getElementById('rent-field');
     const rentFieldString = rentField.value;
     const rentFieldNumber = parseInt(rentFieldString);
 
+    if(isNaN(rentFieldNumber)){
+        alert('Please provide a valid number');
+        return;
+    }
+
     // Clothes Field 
     const clothesField = document.getElementById('clothes-field');
     const clothesFieldString = clothesField.value;
     const clothesFieldNumber = parseInt(clothesFieldString);
+
+    if(isNaN(clothesFieldNumber)){
+        alert('Please provide a valid number');
+        return;
+    }
 
     // calculate
     const totalCost = foodFieldNumber + rentFieldNumber + clothesFieldNumber;
@@ -47,6 +67,11 @@ document.getElementById('btn-save').addEventListener('click', function(){
     const percentageFieldNumber = parseInt(percentageFieldString);
 
     const percentage = (percentageFieldNumber * incomeFieldNumber) / 100;
+
+    if(isNaN(percentage)){
+        alert('Please provide a valid number');
+        return;
+    }
 
     const savingAmount = document.getElementById('saving-amount');
     savingAmount.innerText = percentage;
